ece6203f5fe44b641883171a3114f424ae117527
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-09-28 16:27:54 +0200

    Update imageModel.js

--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose')
 
+const imagesSchema = new mongoose.Schema({
+
+    filename:{
+        type:String,
+        required:true
+    },
+    filepath:{
+        type:String,
+        required:true
+    }
+
+},{ timestamps:true })
+
+
+module.exports = mongoose.model("ImageModel", imagesSchema);
