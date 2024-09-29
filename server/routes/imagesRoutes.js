cc0e58ed455de7785cde7b3f4b558bd574cf3acf
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-09-29 15:51:26 +0200

    Update imagesRoutes.js

--- server/routes/imagesRoutes.js
+++ server/routes/imagesRoutes.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 
 const {viewAllImages} = require('../controllers/imageController');
-const {upload} = require('../controllers/imageController');
-// const {uploadImages} = require('../controllers/imageController');
+// const {upload} = require('../controllers/imageController');
+const {uploadImages} = require('../controllers/imageController');
 
 const router = express.Router();
 
 router.get('/', viewAllImages);
-// router.post('/', uploadImages);
-router.post('/', upload);
-
+router.post('/', uploadImages);
+// router.post('/', upload);
 
 module.exports = router
